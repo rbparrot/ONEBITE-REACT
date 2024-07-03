13313cc5e8321c5ee274e0e670e0a1375fc5e894
Parrot <hy04240@gmail.com>
2024-07-03 19:14:17 +0900

    new

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -6,4 +6,16 @@ age = 30;
 
 // 2. 상수
 const birth = "1997.01.07";
-birth = 123;
+
+// 3. 변수 명명 규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+
+// 3-2. 숫자로 시작할 수 없다.
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드 (가독성)
+
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
